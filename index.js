4c26442a4897f6f3873f4edcda8cb98d9797b196
Alex Kibe <alex.kibe01@student.moringaschool.com>
2023-03-23 05:19:58 +0300

    Completed assignmet

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+function upperCaseCustomerName() {
+    window.customerName = window.customerName.toUpperCase();
+  }
+
+function setBestCustomer(){
+    window.bestCustomer = "not bob";
+}
+
+
+function overwriteBestCustomer() {
+    window.bestCustomer = 'maybe bob';
+  }
+  
+  function changeLeastFavoriteCustomer() {
+    const leastFavoriteCustomer = "Alex";
+  
+    leastFavoriteCustomer = "Alex";
+  }
